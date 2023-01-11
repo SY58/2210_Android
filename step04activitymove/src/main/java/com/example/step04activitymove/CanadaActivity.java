27c92c4a3baad12ffb672115d9039e0e62ff09c4
syCHOI <sychoi.me@gmail.com>
2023-01-11 13:04:52 +0900

    step04activitymove 에 activity 종료 버튼 구현

--- step04activitymove/src/main/java/com/example/step04activitymove/CanadaActivity.java
+++ step04activitymove/src/main/java/com/example/step04activitymove/CanadaActivity.java
@@ -5,14 +5,19 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CanadaActivity extends AppCompatActivity{
-
+public class CanadaActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canada);
-
+        //버튼의 참조값 얻어와서 바로 리스너 등록하기
+        findViewById(R.id.finishBtn).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        //이 액티비티를 종료시키기
+        finish();
+    }
 }
