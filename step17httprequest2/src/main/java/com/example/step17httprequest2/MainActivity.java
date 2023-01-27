7e708d9cb773016a4e7f35d465f456261aa7474c
syCHOI <sychoi.me@gmail.com>
2023-01-27 14:45:51 +0900

    Step17 http request 예제 수정

--- step17httprequest2/src/main/java/com/example/step17httprequest2/MainActivity.java
+++ step17httprequest2/src/main/java/com/example/step17httprequest2/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements Util.RequestListe
 
     @Override
     public void onFail(int requestId, Map<String, Object> result) {
-
+        //에러 메시지를 읽어와서 EditText에 출력하기
+        String data=(String) result.get("data");
+        editText.setText(data);
     }
 }
