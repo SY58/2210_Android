6af9f7ddd5d23214ad67211ad6a8ca1419eb48b4
syCHOI <sychoi.me@gmail.com>
2023-03-14 11:55:39 +0900

    step25 imageCapture_MyHttpUtil 완성하고 사용하는 구조로 변경중

--- step25imagecapture/src/main/java/com/example/step25imagecapture/DetailActivity.java
+++ step25imagecapture/src/main/java/com/example/step25imagecapture/DetailActivity.java
@@ -1,6 +1,5 @@
 package com.example.step25imagecapture;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -9,11 +8,13 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.step25imagecapture.databinding.ActivityGalleryListBinding;
+import com.bumptech.glide.Glide;
+import com.example.step25imagecapture.databinding.ActivityDetailBinding;
+import com.example.step25imagecapture.util.MyHttpUtil;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -21,93 +22,88 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class GalleryListActivity extends AppCompatActivity implements View.OnClickListener{
-    ActivityGalleryListBinding binding;
-    //서버에서 받아온 갤러리 목록을 저장할 객체
-    List<GalleryDto> list=new ArrayList<>();
-    String sessionId;
+public class DetailActivity extends AppCompatActivity implements MyHttpUtil.RequestListener {
+
+    ActivityDetailBinding binding;
     SharedPreferences pref;
-    GalleryAdapter adapter;
+    String sessionId;
+    GalleryDto dto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //바인딩 객체의 참조값을 필드에 저장
-        binding=ActivityGalleryListBinding.inflate(getLayoutInflater());
-        //바인딩 객체를 이용해서 화면 구성
+        setContentView(R.layout.activity_detail);
+        binding=ActivityDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //ListView에 연결할 아답타 객체 생성
-        adapter=new GalleryAdapter(this, R.layout.listview_cell, list);
-        //ListView에 아답타 연결하기
-        binding.listView.setAdapter(adapter);
-        //버튼에 리스너 등록하기
-        binding.takePicBtn.setOnClickListener(this);
-        binding.refreshBtn.setOnClickListener(this);
-        //ListView 의 cell을 클릭했을 때 리스너
-        binding.listView.setOnItemClickListener((parent, view, position, id) -> {
-            //position은 클릭한 cell의 인덱스 값이다.
-            GalleryDto dto=list.get(position);
-            Intent intent=new Intent(this, DetailActivity.class);
-            intent.putExtra("dto",dto);
-            startActivity(intent);
+        //DetailActivity가 활성화될 때 전달받은 Intent 객체의 참조값 얻어오기
+        //GalleryListActivity 에서 생성한 Intent 객체이기 때문에 "dto" 라는 키값으로 GalleryDto 객체가 들어있다.
+        Intent intent=getIntent();
+        dto=(GalleryDto)intent.getSerializableExtra("dto");
+
+        //이미지 출력(Glide 활용)
+        Glide.with(this)
+                .load(dto.getImagePath())
+                .centerCrop()
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(binding.imageView);
+        //세부 정보 출력
+        binding.writer.setText("writer:"+dto.getWriter());
+        binding.caption.setText(dto.getCaption());
+        binding.regdate.setText(dto.getRegdate());
+
+        pref= PreferenceManager.getDefaultSharedPreferences(this);
+        sessionId=pref.getString("sessionId", "");
+
+        //삭제 버튼에 리스너 등록
+        binding.deleteBtn.setOnClickListener(v -> {
+            new AlertDialog.Builder(this)
+                    .setMessage("삭제 하시겠습니까?")
+                    .setPositiveButton("네", (dialog, which) -> {
+                        //삭제할 갤러리 사진의 Primary key를 이용해서 삭제 작업을 진행한다.
+                        int num=dto.getNum();
+
+                    })
+                    .setNegativeButton("아니요", null)
+                    .create()
+                    .show();
         });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-        pref= PreferenceManager.getDefaultSharedPreferences(this);
-        sessionId=pref.getString("sessionId", "");
-
-        //원격지 서버로부터 갤러리 목록을 받아오는 요청을 한다.
-        new GalleryListTask().execute(AppConstants.BASE_URL+"/api/gallery/list");
+        //우리의 서버에 root 요청
+        new MyHttpUtil(this).sendGetRequest(1,
+                AppConstants.BASE_URL+"/", null, this);
     }
 
-    //버튼을 눌렀을때 호출되는 메소드
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.takePicBtn:
-                //사진을 찍어서 올리는 액티비티를 실행한다.
-                Intent intent=new Intent(this, MainActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.refreshBtn:
-                //목록을 다시 받아온다.
-                new GalleryListTask().execute(AppConstants.BASE_URL+"/api/gallery/list");
-                break;
-        }
+    public void onSuccess(int requestId, String data) {
+        Log.d("테스트", data);
     }
 
-    //갤러리 목록을 얻어올 작업을 할 비동기 task
-    class GalleryListTask extends AsyncTask<String, Void, String> {
+    @Override
+    public void onFail(int requestId, Map<String, Object> result) {
 
-        //진행중 알림을 띄우기 위한 객체
-        ProgressDialog progress=new ProgressDialog(GalleryListActivity.this);
+    }
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            //진행중 알림을 띄운다.
-            progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            progress.setMessage("로딩중입니다...");
-            progress.show();
-        }
+    //로그인 여부를 체크하는 작업을 할 비동기 task
+    class LoginCheckTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... strings) {
-            //요청 url
+            //로그인 체크 url
             String requestUrl=strings[0];
             //서버가 http 요청에 대해서 응답하는 문자열을 누적할 객체
             StringBuilder builder=new StringBuilder();
             HttpURLConnection conn=null;
             InputStreamReader isr=null;
             BufferedReader br=null;
+            boolean isLogin=false;
             try{
                 //URL 객체 생성
                 URL url=new URL(requestUrl);
@@ -143,7 +139,7 @@ public class GalleryListActivity extends AppCompatActivity implements View.OnCli
                 }
                 //서버가 응답한 쿠키 목록을 읽어온다.
                 List<String> cookList=conn.getHeaderFields().get("Set-Cookie");
-                //만일 쿠키가 존대 한다면
+                //만일 쿠키가 존재한다면
                 if(cookList != null){
                     //반복문 돌면서
                     for(String tmp : cookList){
@@ -157,13 +153,13 @@ public class GalleryListActivity extends AppCompatActivity implements View.OnCli
                             editor.putString("sessionId", sessionId);
                             editor.apply();//apply() 는 비동기로 저장하기 때문에 실행의 흐름이 잡혀 있지 않다(지연이 없음)
                             //필드에도 담아둔다.
-                            GalleryListActivity.this.sessionId=sessionId;
+                            DetailActivity.this.sessionId=sessionId;
                         }
                     }
                 }
 
             }catch(Exception e){//예외가 발생하면
-                Log.e("MusicListTask", e.getMessage());
+                Log.e("LoginCheckTask", e.getMessage());
             }finally {
                 try{
                     if(isr!=null)isr.close();
@@ -171,44 +167,33 @@ public class GalleryListActivity extends AppCompatActivity implements View.OnCli
                     if(conn!=null)conn.disconnect();
                 }catch(Exception e){}
             }
-            //응답받은 문자열을 리턴한다.
+            //서버에서 응답받은 문자열을 리턴한다.
             return builder.toString();
         }
 
         @Override
         protected void onPostExecute(String jsonStr) {
             super.onPostExecute(jsonStr);
-            //여기는 UI 스레드 (자유롭게 UI작업을 할수있다)
-            //jsonStr 은 [{},{},...] 형식의 문자열이기 때문에 JSONArray 객체를 생성한다.
-            list.clear();
-            try {
-                JSONArray arr=new JSONArray(jsonStr);
-                for(int i=0; i<arr.length(); i++){
-                    //i번째 JSONObject 객체를 참조
-                    JSONObject tmp=arr.getJSONObject(i);
-                    int num=tmp.getInt("num");
-                    String writer=tmp.getString("writer");
-                    String caption=tmp.getString("caption");
-                    String imagePath=tmp.getString("imagePath");
-                    String regdate=tmp.getString("regdate");
-                    GalleryDto dto=new GalleryDto();
-                    dto.setNum(num);
-                    dto.setWriter(writer);
-                    dto.setCaption(caption);
-                    //http://xxx/xxx/resources/upload/xxx.jpg 형식의 문자열을 구성해서 넣기
-                    dto.setImagePath(AppConstants.BASE_URL+imagePath);
-                    dto.setRegdate(regdate);
-                    //ArrayList 객체에 누적시키기
-                    list.add(dto);
-                }
-                //모델의 데이터가 바뀌었다고 아답타에 알려서 listView가 업데이트 되도록 한다.
-                adapter.notifyDataSetChanged();
 
-            } catch (JSONException je) {
-                Log.e("onPostExecute()", je.getMessage());
+            try {
+                //json문자열을 이용해서 JSONObject 객체를 생성한다.
+                JSONObject obj=new JSONObject(jsonStr);
+                //로그인 여부
+                boolean isLogin=obj.getBoolean("isLogin");
+                if(isLogin){
+                    //로그인된 아이디를 읽어와서
+                    String id=obj.getString("id");
+                    //갤러리 writer와 비교해서 같으면 삭제 버튼을 보이게 한다.
+                    if(id.equals(dto.getWriter())){
+                        //삭제 버튼 보이게하기
+                        binding.deleteBtn.setVisibility(View.VISIBLE);
+                    }
+
+                }
+            }catch (JSONException je){
+                Log.e("onPostExecute", je.getMessage());
             }
-            //프로그레스 취소
-            progress.dismiss();
         }
     }
 }
+
